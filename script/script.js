2f75a37baa7a445ec1fd93269a3ea69a14fb306a
Jasmine-Lapierre <89647613+Jasmine-Lapierre@users.noreply.github.com>
2023-04-14 09:42:45 -0400

    yipppie!

--- script/script.js
+++ script/script.js
@@ -5,3 +5,15 @@ let anim= gsap.to(".action", {
     repeat:-1
 })
 anim.play()
+
+let spritesheet = document.querySelector(".spritesheetbody")
+
+function minuteur(){
+document.querySelector("body").classList.remove('is-scrolling')
+}
+
+window.addEventListener("scroll", ()=>{
+document.querySelector("body").classList.add('is-scrolling');
+setTimeout(minuteur,100);
+});
+
