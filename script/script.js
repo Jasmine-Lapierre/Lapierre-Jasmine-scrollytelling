8a2995c554274101561cfb132f1e9e6baac92943
Jasmine-Lapierre <89647613+Jasmine-Lapierre@users.noreply.github.com>
2023-04-27 21:58:26 -0400

    travail

--- script/script.js
+++ script/script.js
@@ -4,8 +4,30 @@ let anim= gsap.to(".action", {
     duration:1.5,
     repeat:-1
 })
+let animarbre= gsap.to(".arbre", {
+    x:"-200vw",
+    duration:10,
+    repeat:-1
+})
+let animarbreseul = gsap
+  .timeline({ })
+  .to(
+    document.querySelector('.arbrechat'),
+    {
+      x: "-100vw",
+      duration:3,
+    }
+  )
+  .to(
+    document.querySelector('.chatrosesaute'),
+    {
+    x:"-30vw",
+      y: "15vw",
+      scale:2
+    }
+  )
 anim.play()
-
+animarbreseul.play()
 let spritesheet = document.querySelector(".spritesheetbody")
 let body = document.querySelector("body")
 let timer
